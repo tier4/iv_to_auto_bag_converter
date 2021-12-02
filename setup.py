d3f60d9be4dd6de7e8301b8a85cd5aaa6483baf6
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2021-12-02 10:32:54 +0900

    feat: create pkg

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'iv_to_auto_bag_converter'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hyt',
+    maintainer_email='hayato-m126@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
